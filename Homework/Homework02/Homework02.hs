d7a24da4772d5942e52a203bd1a51c0944bb5c23
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-03-16 16:03:22 +0100

    hw2

--- Homework/Homework02/Homework02.hs
+++ Homework/Homework02/Homework02.hs
@@ -19,15 +19,31 @@ f4 x y z = x == (y ++ z)
 
 -- Question 2
 -- Why should we define type signatures of functions? How can they help you? How can they help others?
+-- It helps understanding what type of data the function takes and what type it provides.
 
 
 -- Question 3
 -- Why should you define type signatures for variables? How can they help you?
+-- It helps to kow what kind of data it is so what operations can be done with it.
 
 
 -- Question 4
 -- Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
+--Yes, it's called coercion functions ; e.g.
+-- * fromIntegral :: Int | Integer -> Float | Double | etc.
+-- * realToFrac :: Real (= Float | Double | Rational) -> Fractional
+-- * toEnum / fromEnum :: Enum (Char | Bool | Ordering) <-> Int
+-- * show / read
+--   show :: Show a => a -> String
+--   read :: Read a => String -> a
 
 -- Question 5
 -- Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner
 -- most elements?
+
+-- Define list of lists :
+x :: [[Bool]]
+x = [[True, False], [False, False]]
+
+-- Access inner elements :
+y = x !! 1 !! 0
