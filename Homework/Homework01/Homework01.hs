9ff97401fb9858c871bf8ad97f598a15d5a9570c
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-03-16 15:11:49 +0100

    Homework01

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -1,16 +1,23 @@
 
 -- Question 1
 -- Write a multiline comment below.
+{-
+    Hello!
+-}
 
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
+mulT x = x * 3
 
 -- Question 3
 -- Define a function that calculates the area of a circle.
+areaCircle r = pi * r * r
 
 -- Question 4
--- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+-- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder.
+volumeCylinder r h = areaCircle r * h
 
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
+greaterEq42 r h = volumeCylinder r h > 42
 
