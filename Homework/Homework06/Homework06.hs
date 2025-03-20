97108cdc887623ff1ca2359dcf5b7da776b68760
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-03-20 23:31:50 +0100

    hw6 - reimplementations

--- Homework/Homework06/Homework06.hs
+++ Homework/Homework06/Homework06.hs
@@ -2,6 +2,9 @@
 -- Write a function called `repeat'` that takes a value and creates an infinite list with
 -- the value provided as every element of the list.
 --
+
+repeat' :: a -> [a]
+repeat' value = value : repeat' value
 -- >>> repeat 17
 --[17,17,17,17,17,17,17,17,17...
 
@@ -10,7 +13,9 @@
 -- Using the `repeat'` function and the `take` function we defined in the lesson (comes with Haskell),
 -- create a function called `replicate'` that takes a number `n` and a value `x` and creates a list
 -- of length `n` with `x` as the value of every element. (`n` has to be Integer.)
---
+
+replicate' :: Int -> a -> [a]
+replicate' n x = take n (repeat' x)
 -- >>> replicate 0 True
 -- []
 -- >>> replicate (-1) True
@@ -21,7 +26,11 @@
 
 -- Question 3
 -- Write a function called `concat'` that concatenates a list of lists.
---
+concat' :: [[a]] -> [a]
+--concat' listOfLists = map (\list -> foldl (
+--concat' listOfLists = foldr (++) [] listOfLists
+concat' = foldl (++) []
+
 -- >>> concat' [[1,2],[3],[4,5,6]]
 -- [1,2,3,4,5,6]
 
@@ -29,7 +38,14 @@
 -- Question 4
 -- Write a function called `zip'` that takes two lists and returns a list of
 -- corresponding pairs (zips them) like this:
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+--zip' l1 l2 =
 --
+--listToPair (x:xl) = (x, listToPair xl)
 -- >>> zip' [1, 2] ['a', 'b']
 -- [(1,'a'),(2,'b')]
 --
@@ -50,6 +66,11 @@
 -- Question 5
 -- Create a function called `zipWith'` that generalises `zip'` by zipping with a
 -- function given as the first argument, instead of a tupling function.
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' f _ [] = []
+zipWith' f [] _ = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 --
 -- > zipWith' (,) xs ys == zip' xs ys
 -- > zipWith' f [x1,x2,x3..] [y1,y2,y3..] == [f x1 y1, f x2 y2, f x3 y3..]
@@ -65,6 +86,9 @@
 -- Write a function called `takeWhile'` that takes a precate and a list and
 -- returns the list up until an element that doesn't satisfy the predicate.
 --
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' f [] = []
+takeWhile' f (x:xs) = if f x then x : takeWhile f xs else []
 -- >>> takeWhile (< 3) [1,2,3,4,1,2,3,4]
 -- [1,2]
 -- >>> takeWhile (< 9) [1,2,3]
@@ -76,6 +100,16 @@
 -- Question 7 (More difficult)
 -- Write a function that takes in an integer n, calculates the factorial n! and
 -- returns a string in the form of 1*2* ... *n = n! where n! is the actual result.
+
+--strFactorial :: Int -> Bool -> String
+--strFactorial n isInit = strFactorial (n-1) False ++ "*" ++ (show n) ++ if isInit then "=" ++ (show n) ++ "!" else ""
+
+strFactorial :: Int -> String
+strFactorial n = concatMap (\x -> show x ++ "*") [1..n-1] ++ show n ++ " = " ++ show (factorial n) ++ "!"
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
 
 
 -- Question 8
@@ -100,3 +134,8 @@ orderList =
 
 deliveryCost :: Double
 deliveryCost = 8.50
+
+calculateOrder (orderName, orderQty) = (\(_,xp) -> xp * orderQty + deliveryCost) $ (filter (\(xn,_) -> orderName == xn) bevogBeerPrices !! 0)
+
+--ghci> calculateOrder (orderList!!0)
+--38.5
