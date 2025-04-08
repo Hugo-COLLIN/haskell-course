d18e35610508a6648d81cbd737fc4412fe561bbb
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-04-08 12:20:36 +0200

    hw13 - returns a Map

--- Homework/Homework13/Main.hs
+++ Homework/Homework13/Main.hs
@@ -2,15 +2,15 @@ import System.Directory (listDirectory)
 --import Data.List (isInfixOf, sort) -- import only useful functions. It avoids polluting our program with function names we won't use.
 import Data.List hiding (find)
 import qualified Data.Map -- conflit de noms entre 2 imports : qualified permet de forcer à spécifier Data.map.filter pour résoudre le conflit
---x = Data.Map.filter
 
 -- >>> listDirectory "."
 
-find :: String -> IO [FilePath]
+find :: String -> IO (Data.Map.Map Int String)
 find searchTerm = do
     entries <- listDirectory "."
     let found = sort $ filter (searchTerm `isInfixOf`) entries
-    return found
+    let foundMap = Data.Map.fromList $ zip ([1..] :: [Int]) found
+    return foundMap
 
 main = do
     putStrLn "Provide search term: "
