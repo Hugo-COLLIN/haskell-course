1cb88de9c17971e9a4dc50b6ddd692401b51e70c
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-04-08 12:24:57 +0200

    hw13 - managing imports

--- Homework/Homework13/Main.hs
+++ Homework/Homework13/Main.hs
@@ -1,15 +1,18 @@
 import System.Directory (listDirectory)
 --import Data.List (isInfixOf, sort) -- import only useful functions. It avoids polluting our program with function names we won't use.
 import Data.List hiding (find)
-import qualified Data.Map -- conflit de noms entre 2 imports : qualified permet de forcer à spécifier Data.map.filter pour résoudre le conflit
+
+-- conflit de noms entre 2 imports : qualified permet de forcer à spécifier Data.map.filter pour résoudre le conflit
+import qualified Data.Map as Map hiding (Map)
+import Data.Map (Map) -- avoids to write Map.Map
 
 -- >>> listDirectory "."
 
-find :: String -> IO (Data.Map.Map Int String)
+find :: String -> IO (Map Int String)
 find searchTerm = do
     entries <- listDirectory "."
     let found = sort $ filter (searchTerm `isInfixOf`) entries
-    let foundMap = Data.Map.fromList $ zip ([1..] :: [Int]) found
+    let foundMap = Map.fromList $ zip ([1..] :: [Int]) found
     return foundMap
 
 main = do
