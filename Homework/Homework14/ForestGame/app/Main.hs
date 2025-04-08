603455f4b22f52051df8b0028aadb9c4722c4281
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-04-08 21:42:44 +0200

    hw14 - using language extensions

--- Homework/Homework14/ForestGame/app/Main.hs
+++ Homework/Homework14/ForestGame/app/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
 import Forest.Level1 (Forest(..), level1forest) --helperFunction won't work because not exposed
@@ -6,7 +9,7 @@ import System.Random (randomRIO)
 
 main :: IO ()
 main = do
-        startingStamina <- randomRIO (10000,20000) :: IO Int
+        startingStamina <- randomRIO @Int (10_000,20_000) -- @Int allowed by TypeApplications
         putStrLn "You're traped in a Forest, try to scape! Remember that you loose stamina with each step you take"
         gameLoop (startingStamina, level1forest)
     where
@@ -15,4 +18,4 @@ main = do
         gameLoop (s, forest) = do
             putStrLn $ "You have " ++ show s ++ " stamina, and you're still inside the Forest. Choose a path, brave adventurer: GoLeft, GoRight, GoForward!"
             selectedMove <- getLine
-            gameLoop $ move (s, forest) (read selectedMove :: AvailableMoves)
+            gameLoop $ move (s, forest) (read @AvailableMoves selectedMove)
--- Homework/Homework14/ForestGame/src/Forest/Level1.hs
+++ Homework/Homework14/ForestGame/src/Forest/Level1.hs
@@ -1,4 +1,5 @@
--- 1 module per file
+{-# LANGUAGE NumericUnderscores #-}
+
 module Forest.Level1 (Forest(..), level1forest) where
 
 data Forest a = Exit | Trail a (Forest a) (Forest a) (Forest a) deriving (Show)
@@ -6,9 +7,9 @@ data Forest a = Exit | Trail a (Forest a) (Forest a) (Forest a) deriving (Show)
 level1forest :: (Ord a, Num a) => Forest a
 level1forest =
     Trail
-        3000
+        3_000 -- allowed by NumericUnderscores
         ( Trail
-            7000
+            7_000
             (Trail 3000 Exit Exit Exit)
             (Trail 4000 Exit Exit Exit)
             (Trail 5000 Exit Exit Exit)
