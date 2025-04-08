235e5b5ec3bde044fa7bfcd55123909f21be9cd0
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-04-08 12:15:41 +0200

    hw13 - imports names conflicts

--- Homework/Homework13/Main.hs
+++ Homework/Homework13/Main.hs
@@ -1,17 +1,20 @@
-import System.Directory
-import Data.List
+import System.Directory (listDirectory)
+--import Data.List (isInfixOf, sort) -- import only useful functions. It avoids polluting our program with function names we won't use.
+import Data.List hiding (find)
+import qualified Data.Map -- conflit de noms entre 2 imports : qualified permet de forcer à spécifier Data.map.filter pour résoudre le conflit
+--x = Data.Map.filter
 
 -- >>> listDirectory "."
 
-find' :: String -> IO [FilePath]
-find' searchTerm = do
+find :: String -> IO [FilePath]
+find searchTerm = do
     entries <- listDirectory "."
-    let found = filter (searchTerm `isInfixOf`) entries
+    let found = sort $ filter (searchTerm `isInfixOf`) entries
     return found
 
 main = do
     putStrLn "Provide search term: "
     searchTerm <- getLine
-    found <- find' searchTerm
+    found <- find searchTerm
     putStrLn $ "These entries match your search: " ++ show found
 
